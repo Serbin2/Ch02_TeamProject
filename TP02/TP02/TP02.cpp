3b83dd4abaf2e4414480d3a97e44063634c4a32a
minseo kang <105419762+frieanfkerfaerjfeaferf@users.noreply.github.com>
2026-03-25 22:44:09 +0900

    연습용

--- TP02/TP02/TP02.cpp
+++ TP02/TP02/TP02.cpp
@@ -3,11 +3,15 @@
 //	260325 지호나
 
 #include "Graphics/ConsoleGraphic.h"
+#include "Player.h"
+
 
 // [2026-03-25, 박재현] 권한 테스트 2
 
 int main()
 {
+	system("chcp 65001");
+
 	std::cout << "윈도우 10 이상의 콘솔창은 코드에서의 콘솔 창 크기 강제 조절이 동작하지 않을 수 있습니다.\n";
 	std::cout << "게임을 시작하기 전에 콘솔 창 크기를 적절하게 조절 한 후 진행하시기 바랍니다.\n";
 	std::cout << "아무 키를 눌러 계속합니다.\n";
@@ -21,6 +25,12 @@ int main()
 	///	게임에 필요한 객체들 준비 구간
 	CGraphic* graphinc = CGraphic::GetInstance();
 
+	/// player 만들기
+	CPlayer* player = new CPlayer("강민서");
+	
+	player->ShowName();
+
+
 	if (graphinc == nullptr)
 	{
 		return 0;
@@ -33,7 +43,7 @@ int main()
 
 	while (1)
 	{
-
+		player->ShowName();
 	}
 
 
