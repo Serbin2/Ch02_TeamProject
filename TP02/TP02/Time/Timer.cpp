4461f1a113793d12b176300f7a107863e013682d
JIHONA\njh10 <njh1026@gmail.com>
2026-03-26 12:52:59 +0900

    timer 수정중

--- TP02/TP02/Time/Timer.cpp
+++ TP02/TP02/Time/Timer.cpp
@@ -5,6 +5,12 @@
 CTimer::CTimer()
 {
 	m_bPaused = false;
+	m_iTargetFPS = 60;
+	m_dFrameDuration = duration<double>(1.0 / m_iTargetFPS);
+
+	m_dFpsTimer = 0.0f;
+	m_iFpsCount = 0;
+	m_iFps = 0;
 }
 
 CTimer::~CTimer()
@@ -14,19 +20,44 @@ CTimer::~CTimer()
 
 void CTimer::Start()
 {
-	m_dPrev = high_resolution_clock::now();
+	m_dPrev = steady_clock::now();
+	m_dFpsTimer = 0.0f;
+	m_iFpsCount = 0;
+	m_iFps = 0;
 }
 
 void CTimer::Pause() 
 {
+	m_bPaused = true;
+	m_dPausedTime = steady_clock::now();
+}
 
+void CTimer::Resume()
+{
+	if (!m_bPaused)	return;
+
+	m_dPrev += steady_clock::now() - m_dPausedTime;
+	m_bPaused = false;
 }
 
 double CTimer::Update()
 {
-	auto now = high_resolution_clock::now();
+	if (m_bPaused)	return 0.0;
+
+	if (m_iTargetFPS > 0)
+	{
+
+	}
+
+	auto now = steady_clock::now();
 	auto elap = now - m_dPrev;
 	double delta = duration<double>(elap).count();
 	m_dPrev = now;
 	return delta;
+}
+
+void CTimer::LimitFrame(duration<double> target)
+{
+	auto now = steady_clock::now();
+	auto elap = steady_clock::now();
 }
--- TP02/TP02/Time/Timer.h
+++ TP02/TP02/Time/Timer.h
@@ -12,12 +12,24 @@ public:
 
 	void Start();
 	void Pause();
+	void Resume();
 	double Update();
 	
 
 private:
 	
+	void LimitFrame(duration<double> target);
+
 	steady_clock::time_point m_dPrev;
+	steady_clock::time_point m_dPausedTime;
+
+	duration<double> m_dFrameDuration;
+
+	int m_iTargetFPS;
+
+	double m_dFpsTimer;
+	int m_iFpsCount;
+	int m_iFps;
 
 	bool m_bPaused;
 };
