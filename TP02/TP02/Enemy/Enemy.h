867ec56048f81cb52d545592308228d9f1e32e9d
seonjae01 <tjswo5536@naver.com>
2026-03-27 21:02:09 +0900

    [feat: Enemy 뼈대 구성 및 랜덤 이동 구현]

---
+++ TP02/TP02/Enemy/Enemy.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "../Character/Character.h"
+
+class CEnemy : public CCharacter
+{
+	CEnemy(int Shape, int Color);
+	~CEnemy() = default;
+
+	virtual void Tick(double DeltaTime) override;
+	virtual void Move() override;
+	virtual void Attack(COORD Direction) override;
+	virtual void OnHit(float Damage) override;
+};
