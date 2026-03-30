29ccd76c7dfb93769d885561b47a1978cd525d30
minseo kang <105419762+frieanfkerfaerjfeaferf@users.noreply.github.com>
2026-03-30 16:56:18 +0900

    [feat. 강민서 상점 오류 엔터치지 않고 넘어가기]

--- TP02/TP02/Inventory/Inventory.cpp
+++ TP02/TP02/Inventory/Inventory.cpp
@@ -19,7 +19,7 @@ CInventory::CInventory()
 	//UpdateUI();
 
 #ifdef _DEBUG
-		AddItem(std::make_shared<Potion>(), 3);
+	AddItem(std::make_shared<Potion>(), 3);
 #endif
 }
 
@@ -78,7 +78,7 @@ void CInventory::UseItem(int iItemIdx, std::weak_ptr<class CPlayer> pPlayer)
 	int iSize = static_cast<int>(m_vSlots.size());
 
 	// 잘못된 인덱스 접근
-	if (iItemIdx < 0 ||  iSize <= iItemIdx)
+	if (iItemIdx < 0 || iSize <= iItemIdx)
 	{
 		return;
 	}
