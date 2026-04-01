353717c3ebe734dfb9395c24ea00589e8f9280c7
DESKTOP-Q7PO5NC\dhy <pdh060612@gmail.com>
2026-04-01 11:24:42 +0900

    [feat: 애니메이션]
    애니메이션 메인

--- TP02/TP02/FrontMenu/MainMenu.h
+++ TP02/TP02/FrontMenu/MainMenu.h
@@ -1,20 +1,25 @@
 #pragma once
 #include <iostream>
+#include <string>
 
-class CMainMenu {
+class CMainMenu
+{
 private:
 	enum eScene { MENU, START, CREDIT, EXIT };
 	eScene scene = MENU;
 
-	int iCount= 0;
+	int iCount = 0;
 	int in = 0;
+	float fLightAngle = 0.0f;
 
 	const char* cTitle;
 	const char* cCredits;
+	std::string cBaseArt;
 
 	void vChoice();
 	void vCredit();
 	void vScene();
+	void vDrawLitArt();
 
 public:
 	CMainMenu();
